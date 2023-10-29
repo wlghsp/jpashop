1118794d1bb73aae6ee4adef4c8537b5db4ab81f
JIHO CHOI <wlghsp@gmail.com>
2023-10-29 10:00:39 +0900

    상품 엔티티 개발(비즈니스 로직 추가), 상품 리포지토리 개발, 상품 서비스 개발

---
+++ src/main/java/jpabook/jpashop/domain/service/ItemService.java
@@ -0,0 +1,31 @@
+package jpabook.jpashop.domain.service;
+
+import jpabook.jpashop.domain.item.Item;
+import jpabook.jpashop.domain.repository.ItemRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+@Service
+public class ItemService {
+
+    private final ItemRepository itemRepository;
+
+    @Transactional
+    public void saveItem(Item item) {
+        itemRepository.save(item);
+    }
+
+    public List<Item> findItems() {
+        return itemRepository.findAll();
+    }
+
+    public Item findOne(Long itemId) {
+        return itemRepository.findOne(itemId);
+    }
+
+}
