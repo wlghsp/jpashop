996a335c631c7e396c1ffb40278aa41f39e7ded5
JIHO CHOI <wlghsp@gmail.com>
2023-10-30 23:41:54 +0900

    홈 화면과 레이아웃, 회원 등록, 회원 목록 조회, 상품 등록, 상품 목록, 상품 수정, 변경감지와 병합(merge), 상품 주문, 주문 목록 검색, 취소

--- src/main/java/jpabook/jpashop/HelloController.java
+++ src/main/java/jpabook/jpashop/HelloController.java
@@ -1,15 +1,17 @@
 package jpabook.jpashop;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@Slf4j
 @Controller
 public class HelloController {
 
-    @GetMapping("hello")
+    @GetMapping("/")
     public String hello(Model model) {
-        model.addAttribute("data", "hello!!!");
-        return "hello";
+        log.info("home controller");
+        return "home";
     }
 }
