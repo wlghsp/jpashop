c99dfb9c8e95f675745145413a806d7178baa79c
JIHO CHOI <wlghsp@gmail.com>
2023-11-02 18:50:18 +0900

    주문 조회 V3: 엔티티를 DTO로 변환 - 페치 조인 최적화

--- src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -42,6 +42,17 @@ public class OrderRepository {
         ).getResultList();
     }
 
+    public List<Order> findAllWithItem() {
+        return em.createQuery(
+                "select distinct o from Order o" +
+                        " join fetch o.member m" +
+                        " join fetch o.delivery d" +
+                        " join fetch o.orderItems oi" +
+                        " join fetch oi.item i", Order.class)
+                .getResultList();
+    }
+
+
     /**
      * JPQL 로 처리
      * 실무에서 안 씀
